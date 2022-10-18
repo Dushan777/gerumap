561473a70050b53083f458ba6a84160e3534c5ab
dcolic3121rn <dcolic3121rn@raf.rs>
2022-10-18 16:55:27 +0200

    appcore update

--- src/dsw/gerumap/app/AppCore.java
+++ src/dsw/gerumap/app/AppCore.java
@@ -1,4 +1,39 @@
 package dsw.gerumap.app;
 
-public class AppCore {
+import dsw.gerumap.app.core.ApplicationFramework;
+import dsw.gerumap.app.core.Gui;
+import dsw.gerumap.app.gui.swing.SwingGui;
+
+import javax.swing.*;
+
+public class AppCore extends ApplicationFramework{
+
+    private  static AppCore instance;
+
+    private AppCore()
+    {
+
+    }
+
+    public  static  AppCore getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new AppCore();
+        }
+        return instance;
+    }
+
+    public void run()
+    {
+       // this.gui = gui;
+    }
+
+    public static void main(String[] args) {
+      //  Gui gui = new SwingGui();
+       ApplicationFramework appCore = AppCore.getInstance();
+      //  appCore.initialise(gui);
+        appCore.run();
+    }
+
 }
