3b1a33c0a4f2c48364d8bfb3f60828a92ce6123f
dcolic3121rn <dcolic3121rn@raf.rs>
2022-10-18 17:01:59 +0200

    Swing Gui update

--- src/dsw/gerumap/app/gui/swing/view/MainFrame.java
+++ src/dsw/gerumap/app/gui/swing/view/MainFrame.java
@@ -1,4 +1,24 @@
 package dsw.gerumap.app.gui.swing.view;
 
+import dsw.gerumap.app.AppCore;
+
 public class MainFrame {
+
+    private static MainFrame instance;
+
+
+    private MainFrame()
+    {
+
+    }
+
+    public static MainFrame getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new MainFrame();
+        }
+        return instance;
+    }
+
 }
