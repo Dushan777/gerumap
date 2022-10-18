271743ed64fd9f38fa8deafbc1d12e4cf2dfa1ed
ucolic3021rn <ucolic3021rn@raf.rs>
2022-10-18 16:29:17 +0200

    inicijalna postavka projekta

---
+++ src/dsw/gerumap/app/core/ApplicationFramework.java
@@ -0,0 +1,14 @@
+package dsw.gerumap.app.core;
+
+public abstract class ApplicationFramework {
+
+    protected Gui gui;
+
+    public abstract void run();
+
+    public void initialise(Gui gui)
+    {
+        this.gui = gui;
+    }
+
+}
