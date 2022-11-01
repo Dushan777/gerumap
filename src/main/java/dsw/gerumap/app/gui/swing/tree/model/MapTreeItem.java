a2ea43e43ae8473b46e66f346b8435367468a16e
dcolic3121rn <dcolic3121rn@raf.rs>
2022-11-01 16:09:01 +0100

    maptreeitem dodat

--- src/main/java/dsw/gerumap/app/gui/swing/tree/model/MapTreeItem.java
+++ src/main/java/dsw/gerumap/app/gui/swing/tree/model/MapTreeItem.java
@@ -1,4 +1,27 @@
 package main.java.dsw.gerumap.app.gui.swing.tree.model;
 
-public class MapTreeItem {
+import lombok.Getter;
+import lombok.Setter;
+import main.java.dsw.gerumap.app.mapRepository.composite.MapNode;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+@Getter
+@Setter
+public class MapTreeItem extends DefaultMutableTreeNode {
+
+    private MapNode mapNode;
+
+    public MapTreeItem(MapNode nodeModel) {
+        this.mapNode = nodeModel;
+    }
+
+    @Override
+    public String toString() {
+        return mapNode.getName();
+    }
+
+    public void setName(String name) {
+        this.mapNode.setName(name);
+    }
 }
