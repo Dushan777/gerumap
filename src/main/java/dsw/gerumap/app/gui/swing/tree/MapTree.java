0563c5c6296da7292ffd600de5f9d2a83b737ad1
dcolic3121rn <dcolic3121rn@raf.rs>
2022-11-01 16:13:11 +0100

    dodate klase Maptree

---
+++ src/main/java/dsw/gerumap/app/gui/swing/tree/MapTree.java
@@ -0,0 +1,4 @@
+package main.java.dsw.gerumap.app.gui.swing.tree;
+
+public interface MapTree {
+}
--- src/main/java/dsw/gerumap/app/gui/swing/tree/view/MapTreeView.java
+++ src/main/java/dsw/gerumap/app/gui/swing/tree/view/MapTreeView.java
@@ -1,4 +1,22 @@
 package main.java.dsw.gerumap.app.gui.swing.tree.view;
 
-public class MapTreeView {
+
+import main.java.dsw.gerumap.app.gui.swing.tree.controller.MapTreeCellEditor;
+import main.java.dsw.gerumap.app.gui.swing.tree.controller.MapTreeSelectionListener;
+
+import javax.swing.*;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultTreeModel;
+
+public class MapTreeView extends JTree {
+
+
+    public MapTreeView(DefaultTreeModel defaultTreeModel) {
+        setModel(defaultTreeModel);
+        MapTreeCellRenderer ruTreeCellRenderer = new MapTreeCellRenderer();
+        addTreeSelectionListener((TreeSelectionListener) new MapTreeSelectionListener());
+        setCellEditor(new MapTreeCellEditor(this, ruTreeCellRenderer));
+        setCellRenderer(ruTreeCellRenderer);
+        setEditable(true);
+    }
 }
