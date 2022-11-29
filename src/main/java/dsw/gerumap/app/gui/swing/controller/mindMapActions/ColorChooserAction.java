7683ac3cde4f6f83d8b2e48db483d681e93e8348
dcolic3121rn <dcolic3121rn@raf.rs>
2022-11-29 19:41:25 +0100

    dodati labela i dugme

--- src/main/java/dsw/gerumap/app/gui/swing/controller/mindMapActions/ColorChooserAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/mindMapActions/ColorChooserAction.java
@@ -6,6 +6,7 @@ import dsw.gerumap.app.gui.swing.view.MainFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.concurrent.Flow;
 
 public class ColorChooserAction extends AbstractGerumapAction {
 
@@ -21,19 +22,28 @@ public class ColorChooserAction extends AbstractGerumapAction {
         JFrame frame = new JFrame();
         JColorChooser colorChooser = new JColorChooser();
         JTextField textField = new JTextField(10);
+        JButton button = new JButton("Ok");
 
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension screenSize = kit.getScreenSize();
+        colorChooser.setPreferredSize(new Dimension(100,100));
+
         int screenHeight = screenSize.height;
         int screenWidth = screenSize.width;
         frame.setSize(screenWidth / 2,  screenHeight / 2);
         frame.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         frame.setLocationRelativeTo(MainFrame.getInstance());
         frame.setTitle("Color");
-        frame.add(colorChooser, BorderLayout.SOUTH);
+
+        BoxLayout boxLayout = new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS);
+        frame.getContentPane().setLayout(boxLayout);
+
         frame.add(new JLabel("Unesi debljinu linije: "));
-        frame.add(textField, BorderLayout.NORTH);
+        frame.add(textField);
+        frame.add(colorChooser);
+        frame.add(button);
         frame.setVisible(true);
+
         MainFrame.getInstance().getProjectView().startColorState();
     }
 }
