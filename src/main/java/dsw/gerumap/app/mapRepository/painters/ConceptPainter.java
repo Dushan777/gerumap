064997f016b1b48206fadcf6576e98f97da9eb48
ucolic3021rn <ucolic3021rn@raf.rs>
2022-12-01 17:22:13 +0100

    dodat drawString za pojam ali ne do kraja

--- src/main/java/dsw/gerumap/app/mapRepository/painters/ConceptPainter.java
+++ src/main/java/dsw/gerumap/app/mapRepository/painters/ConceptPainter.java
@@ -12,9 +12,15 @@ public class ConceptPainter extends Painter{
     public void draw(Graphics2D g, Element e) {
         Concept concept = (Concept)e;
         g.setPaint(MainFrame.getInstance().getActionManager().getConfirmAction().getColor());
+        String name = MainFrame.getInstance().getActionManager().getConfirmAction().getText2();
+        if(name != null)
+        {
+            concept.setName(name);
+            g.drawString(concept.getName(), concept.getPosition().x + 25, concept.getPosition().y + 25);
+        }
         g.setStroke(new BasicStroke(MainFrame.getInstance().getActionManager().getConfirmAction().getLineStroke()));
         g.drawOval(concept.getPosition().x, concept.getPosition().y, concept.getHeight(), concept.getWidth());
-
+        //promeniti boju, debljinu linije i tekst da ne bude svima isti, pozicija teksta treba da se stavi da bude unutar elipse
     }
     public ConceptPainter(Element element)
     {
