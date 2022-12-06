629b8650913bb9d0149f37b7310f1469da9d24b1
dcolic3121rn <dcolic3121rn@raf.rs>
2022-12-06 21:16:33 +0100

    multiselekcija iscrtava pravougaonik kad se selektuje opcija select i prevlaci misem

--- src/main/java/dsw/gerumap/app/state/concrete/SelectState.java
+++ src/main/java/dsw/gerumap/app/state/concrete/SelectState.java
@@ -7,14 +7,20 @@ import dsw.gerumap.app.mapRepository.implementation.MindMap;
 import dsw.gerumap.app.mapRepository.painters.ConceptPainter;
 import dsw.gerumap.app.mapRepository.painters.Painter;
 import dsw.gerumap.app.state.State;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.awt.*;
 
+@Getter
+@Setter
 public class SelectState extends State {
 
+    private int x1, y1;
     @Override
     public void misKliknut(MindMapView mindMapView, int x, int y) {
-        MindMap mindMap = mindMapView.getMindMap();
+        x1 = x;
+        y1 = y;
         Painter toBeSelected = null;
         for(Painter p : mindMapView.getPainters())
         {
@@ -39,11 +45,27 @@ public class SelectState extends State {
 
     @Override
     public void misPrevucen(MindMapView mindMapView, int x, int y) {
-
+        int width = x1 - x;
+        int height = y1 - y;
+        if(width < 0)
+            width = width * -1;
+        if(height < 0)
+            height = height * -1;
+        Shape shape;
+        if(x1 < x && y1 < y)                              // kada vuce od gornjeg levog ugla
+             shape = new Rectangle(x1,y1,width,height);
+        else if(x1 > x && y1 < y)                         // kada vuce od gornjeg desnog ugla
+             shape = new Rectangle(x1-width,y1,width,height);
+        else if(x1 < x && y1 > y)                         // kada vuce od donjeg levog ugla
+             shape = new Rectangle(x1,y1-height,width,height);
+        else                                              // kada vuce od donjeg desnog ugla
+             shape = new Rectangle(x,y,width,height);
+        Graphics2D g = (Graphics2D) mindMapView.getGraphics();
+        g.draw(shape);
+        mindMapView.repaint();
     }
 
     @Override
     public void misPusten(MindMapView mindMapView, int x, int y) {
-
     }
 }
