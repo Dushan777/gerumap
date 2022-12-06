2f6eb7e4bdf3532e6eaea5733baf033c951ccd4c
ucolic3021rn <ucolic3021rn@raf.rs>
2022-12-06 20:56:01 +0100

    nakon selektovanja omoguceno je menjanje boje, imena i debljine linije

--- src/main/java/dsw/gerumap/app/gui/swing/controller/mindMapActions/ConfirmAction.java
+++ src/main/java/dsw/gerumap/app/gui/swing/controller/mindMapActions/ConfirmAction.java
@@ -3,6 +3,9 @@ package dsw.gerumap.app.gui.swing.controller.mindMapActions;
 import dsw.gerumap.app.core.ApplicationFramework;
 import dsw.gerumap.app.gui.swing.errorLogger.EventType;
 import dsw.gerumap.app.gui.swing.view.MainFrame;
+import dsw.gerumap.app.gui.swing.view.MindMapView;
+import dsw.gerumap.app.mapRepository.implementation.Concept;
+import dsw.gerumap.app.mapRepository.implementation.Element;
 import dsw.gerumap.app.messageGenerator.MessageGeneratorImplementation;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,14 +28,29 @@ public class ConfirmAction implements ActionListener {
         text2 = MainFrame.getInstance().getActionManager().getColorChooserAction().getTextFieldText().getText();
         dimensionX = text2.length()*9 + 45;
         dimensionY = text2.length()*3/2 + 30;
+        MindMapView mindMapView = (MindMapView)MainFrame.getInstance().getProjectView().getTabbedPane().getSelectedComponent();
+        if(mindMapView.getMapSelectionModel().getSelectedElements() != null)
+        {
+            for(Element element : mindMapView.getMapSelectionModel().getSelectedElements())
+            {
+                if(text.matches("[0-9]+") && text2.matches("[0-9a-zA-Z\\s]+"))
+                {
+                    element.setColor(MainFrame.getInstance().getActionManager().getColorChooserAction().getColorChooser().getColor());
+                    element.setLineStroke(Integer.parseInt(text));
+                    Concept c = (Concept) element;
+                    c.setHeight(dimensionX);
+                    c.setWidth(dimensionY);
+                    element.setName(text2);
+                }
 
+            }
+        }
         if(!text.matches("[0-9]+") || !text2.matches("[0-9a-zA-Z\\s]+"))
         {
             ((MessageGeneratorImplementation) ApplicationFramework.getInstance().getMessageGenerator()).setType(EventType.CANNOT_SET_NAME);
             ApplicationFramework.getInstance().getMessageGenerator().generateMessage();
             MainFrame.getInstance().getActionManager().getColorChooserAction().getTextField().setText("");
             MainFrame.getInstance().getActionManager().getColorChooserAction().getTextFieldText().setText("");
-
             return;
         }
 
