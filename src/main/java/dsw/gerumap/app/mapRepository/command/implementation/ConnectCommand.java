06f8d6f44acdb0753ab0ac03d4f8ec6f13b7e23f
ucolic3021rn <ucolic3021rn@raf.rs>
2022-12-20 15:48:30 +0100

    dodati kosturi komandi

---
+++ src/main/java/dsw/gerumap/app/mapRepository/command/implementation/ConnectCommand.java
@@ -0,0 +1,15 @@
+package dsw.gerumap.app.mapRepository.command.implementation;
+
+import dsw.gerumap.app.mapRepository.command.AbstractCommand;
+
+public class ConnectCommand extends AbstractCommand {
+    @Override
+    public void doCommand() {
+
+    }
+
+    @Override
+    public void undoCommand() {
+
+    }
+}
