5a43eb7b4c20cbfd9f0ab36d6159b3a21a9ee616
ucolic3021rn <ucolic3021rn@raf.rs>
2022-12-20 16:44:49 +0100

    AddConceptCommand zapoceta

--- src/main/java/dsw/gerumap/app/mapRepository/command/implementation/AddConceptCommand.java
+++ src/main/java/dsw/gerumap/app/mapRepository/command/implementation/AddConceptCommand.java
@@ -1,15 +1,31 @@
 package dsw.gerumap.app.mapRepository.command.implementation;
 
+import dsw.gerumap.app.gui.swing.tree.model.MapTreeItem;
 import dsw.gerumap.app.mapRepository.command.AbstractCommand;
+import dsw.gerumap.app.mapRepository.composite.MapNodeComposite;
 
 public class AddConceptCommand extends AbstractCommand {
+
+    private MapTreeItem parent;
+    private MapTreeItem child;
+
+    public AddConceptCommand(MapTreeItem parent, MapTreeItem child) {
+        this.parent = parent;
+        this.child = child;
+    }
+
     @Override
     public void doCommand() {
 
+        if(child == null ||  parent==null) return;
+        parent.add(child);
+        ((MapNodeComposite) parent.getMapNode()).addChild(child.getMapNode());
     }
 
     @Override
     public void undoCommand() {
-
+        if(child == null ||  parent==null) return;
+        child.removeFromParent();
+        ((MapNodeComposite)(parent.getMapNode())).deleteChild(child.getMapNode());
     }
 }
