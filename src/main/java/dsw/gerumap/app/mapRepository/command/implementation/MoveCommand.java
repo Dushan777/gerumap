88674d6ececc45f5edfc33d5d89c72be4a186d4d
dcolic3121rn <dcolic3121rn@raf.rs>
2022-12-22 17:07:04 +0100

    undo - redo za move

--- src/main/java/dsw/gerumap/app/mapRepository/command/implementation/MoveCommand.java
+++ src/main/java/dsw/gerumap/app/mapRepository/command/implementation/MoveCommand.java
@@ -1,15 +1,48 @@
 package dsw.gerumap.app.mapRepository.command.implementation;
 
 import dsw.gerumap.app.mapRepository.command.AbstractCommand;
+import dsw.gerumap.app.mapRepository.implementation.Concept;
+import dsw.gerumap.app.mapRepository.implementation.Element;
+import dsw.gerumap.app.mapRepository.implementation.MindMap;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoveCommand extends AbstractCommand {
+
+    private MindMap mindMap;
+    private List<Element> elements = new ArrayList<>();
+    private int movedX, movedY;
+
+    public MoveCommand(MindMap mindMap, List<Element> elements, int movedX, int movedY) {
+        this.mindMap = mindMap;
+        this.elements.addAll(elements);
+        this.movedX = movedX;
+        this.movedY = movedY;
+    }
+
     @Override
     public void doCommand() {
-
+        for(Element e : elements)
+        {
+            Concept concept = (Concept) e;
+            concept.setPosition(new Point(concept.getOriginalX() + movedX, concept.getOriginalY() + movedY));
+            concept.setOriginalX(concept.getOriginalX() + movedX);
+            concept.setOriginalY(concept.getOriginalY() + movedY);
+        }
     }
 
     @Override
     public void undoCommand() {
 
+        for(Element e : elements)
+        {
+            Concept concept = (Concept) e;
+            concept.setPosition(new Point(concept.getOriginalX() - movedX, concept.getOriginalY() - movedY));
+            concept.setOriginalX(concept.getOriginalX() - movedX);
+            concept.setOriginalY(concept.getOriginalY() - movedY);
+
+        }
     }
 }
--- src/main/java/dsw/gerumap/app/state/concrete/MoveState.java
+++ src/main/java/dsw/gerumap/app/state/concrete/MoveState.java
@@ -1,6 +1,8 @@
 package dsw.gerumap.app.state.concrete;
 
 import dsw.gerumap.app.gui.swing.view.MindMapView;
+import dsw.gerumap.app.mapRepository.command.AbstractCommand;
+import dsw.gerumap.app.mapRepository.command.implementation.MoveCommand;
 import dsw.gerumap.app.mapRepository.composite.MapNode;
 import dsw.gerumap.app.mapRepository.implementation.Concept;
 import dsw.gerumap.app.mapRepository.implementation.Element;
@@ -15,55 +17,55 @@ import java.awt.*;
 public class MoveState extends State {
 
     private int clickedX, clickedY;
+
     @Override
     public void misKliknut(MindMapView mindMapView, int x, int y) {
         clickedX = x;
         clickedY = y;
     }
+
     @Override
     public void misPrevucen(MindMapView mindMapView, int x, int y) {
-        if(mindMapView.getMapSelectionModel().getSelectedElements().isEmpty() && mindMapView.getTimesZoomed() > 1)
-        {
-            for(MapNode e : mindMapView.getMindMap().getChildren())
-            {
-                if(e instanceof Concept)
-                {
+        if (mindMapView.getMapSelectionModel().getSelectedElements().isEmpty() && mindMapView.getTimesZoomed() > 1) {
+            for (MapNode e : mindMapView.getMindMap().getChildren()) {
+                if (e instanceof Concept) {
                     Concept c = (Concept) e;
                     c.setPosition(new Point(c.getOriginalX() + x - clickedX, c.getOriginalY() + y - clickedY));
                 }
             }
-        }
-        else
-        {
+        } else {
             for (Element e : mindMapView.getMapSelectionModel().getSelectedElements()) {
                 Concept c = (Concept) e;
                 c.setPosition(new Point(c.getOriginalX() + x - clickedX, c.getOriginalY() + y - clickedY));
             }
         }
     }
+
     @Override
     public void misPusten(MindMapView mindMapView, int x, int y) {
-        if(mindMapView.getMapSelectionModel().getSelectedElements().isEmpty() && mindMapView.getTimesZoomed() > 1)
-        {
-            for(MapNode e : mindMapView.getMindMap().getChildren())
-            {
-                if(e instanceof Concept)
-                {
+        if (mindMapView.getMapSelectionModel().getSelectedElements().isEmpty() && mindMapView.getTimesZoomed() > 1) {
+            for (MapNode e : mindMapView.getMindMap().getChildren()) {
+                if (e instanceof Concept) {
                     Concept c = (Concept) e;
                     c.setOriginalX(c.getPosition().x);
                     c.setOriginalY(c.getPosition().y);
                 }
             }
-        }
-        else
-        {
+        } else {
             // provera da li se nalazi element tu gde je pusteno
             for (Element e : mindMapView.getMapSelectionModel().getSelectedElements()) {
                 Concept c = (Concept) e;
-                c.setOriginalX(c.getPosition().x);
-                c.setOriginalY(c.getPosition().y);
+                //c.setOriginalX(c.getPosition().x);
+                //c.setOriginalY(c.getPosition().y);
             }
-            mindMapView.getMapSelectionModel().deselectElements();
+
+            int movedX = x - clickedX;
+            int movedY = y - clickedY;
+            System.out.println("movedX = " + movedX);
+            System.out.println("movedY = " + movedY);
+            AbstractCommand abstractCommand = new MoveCommand(mindMapView.getMindMap(), mindMapView.getMapSelectionModel().getSelectedElements(), movedX, movedY);
+            mindMapView.getMindMap().getCommandManager().addCommand(abstractCommand);
         }
+
     }
 }
